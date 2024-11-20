3654c14287fcfc184a8bfa8ecab5bdaac3482cdd
vsbrall143 <vsbrall143@gmail.com>
2024-11-21 02:02:08 +0530

    server file with routing to different pages

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const http= require('http');
-function rqListner(req,res)
-{
-    console.log(req);
-}
+ 
 
  Server=http.createServer((req,res)=>{
-     console.log(req);
+    console.log("----------------started---------------")
+    // console.log(req.url,req.method,req.headers);
+    const url=req.url;
+    if(url =="/home")
+    {
+ 
+        res.write('<html>')
+ 
+        res.write('<body><h1>welcome home<h1> </body>')
+        res.write('</html>')
+  
+    }
+    if(url =="/about")
+        {
+     
+            res.write('<html>')
+     
+            res.write('<body><h1>welcome to about us page<h1> </body>')
+            res.write('</html>')
+ 
+        }
+    if(url =="/node")
+    {
+         
+                res.write('<html>')
+         
+                res.write('<body><h1>welcome to my node js project<h1> </body>')
+                res.write('</html>')
+ 
+    }
+ 
  });
 
 Server.listen(3000);
