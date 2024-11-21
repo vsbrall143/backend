6e934cd592bc2dc2b8da9bb77d976d4a691bdd8f
vsbrall143 <vsbrall143@gmail.com>
2024-11-21 19:37:03 +0530

    this page has takes user input from form and saves the text on the text file created and shows the data form the text file on top of form input in html

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
-const http= require('http');
- 
+const http = require("http");
+const fs = require("fs");
 
- Server=http.createServer((req,res)=>{
-    console.log("----------------started---------------")
-    // console.log(req.url,req.method,req.headers);
-    const url=req.url;
-    if(url =="/home")
-    {
- 
-        res.write('<html>')
- 
-        res.write('<body><h1>welcome home<h1> </body>')
-        res.write('</html>')
-  
-    }
-    if(url =="/about")
-        {
-     
-            res.write('<html>')
-     
-            res.write('<body><h1>welcome to about us page<h1> </body>')
-            res.write('</html>')
- 
+const Server = http.createServer((req, res) => {
+  console.log("----------------started---------------");
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+
+    
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          data = '';
         }
-    if(url =="/node")
-    {
-         
-                res.write('<html>')
-         
-                res.write('<body><h1>welcome to my node js project<h1> </body>')
-                res.write('</html>')
- 
-    }
- 
- });
+  
+        const messages = data.split('\n').reverse();
+  
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body>');
+  
+        messages.forEach(message => {
+          res.write(`<p>${message}</p>`);
+        });
+  
+        res.write('<form action="/message" method="POST">');
+        res.write('<input type="text" name="message">');
+        res.write('<button type="submit">Send</button>');   
+  
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+      });
+  }
 
-Server.listen(3000);
+else if (url === "/message" && method === 'POST') {
+
+    const body=[];
+    req.on('data',(chunk)=>{
+        console.log(chunk);
+        body.push(chunk);
+    });
+    req.on('end',()=>{
+        const parsedBody=Buffer.concat(body).toString();
+        const message=parsedBody.split('=')[1];
+        console.log(message);
+        fs.writeFileSync("message.txt", message);
+    })
+
+    res.writeHead(302, {'Location': '/'}); //this method is used to send http headert to client
+    return res.end();                      //it initiates an http redirect 302 indicate temporary redirect
+                                           // second field is header obj that specifies the
+  }                                        // location to which client is re directed
+
+
+else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not Found');
+  }
+});
+Server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
