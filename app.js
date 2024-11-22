136655dd1bb1f1cf31169e24e3c55ecdd95e9871
vsbrall143 <vsbrall143@gmail.com>
2024-11-23 02:26:28 +0530

    input field added and post request is managed by app.post,both input field text is shown on console body parser is used to parse the form data form user input

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const http = require("http");   //core module import
 const express=require("express");
+const bodyParser=require("body-parser");   //3rd party package used for parsing autometically
+
 
 const app=express();
 
-app.use((req,res,next) => {
-    console.log("in the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));  //shis middleware parsed the form input autometically it has built in next() function
+
+app.use("/add-product", (req,res,next) => {
+ 
+    res.send(' <form action="/product" method="POST"><label for="title">Title:</label> <input type="text" name="title" id="title"> <label for="size">Size:</label> <input type="text" name="size" id="size"><button type="submit">Add Product</button></form>');
+ 
 });
 
-app.use((req,res,next) => {
-    console.log("in another middleware");
+app.post("/product", (req,res,next) => {
+    console.log(req.body);
+    res.redirect("/");
+});
+
+
+app.use("/", (req,res,next) => {
+ 
     res.send('<h1>hello form express</h1>');
 });
 
-const Server = http.createServer(app);
-
-Server.listen(3000, () => {console.log('Server listening on port 3000');});
+app.listen(3000);
