d3cb1b8baa2797591eba5b2c440ba438a336b314
vsbrall143 <vsbrall143@gmail.com>
2024-11-23 00:07:38 +0530

    added express functionality to handle request

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const http = require("http");
-const routes=require('./routes');
+const http = require("http");   //core module import
+const express=require("express");
 
+const app=express();
 
-const Server = http.createServer(routes);
+app.use((req,res,next) => {
+    console.log("in the middleware");
+    next();
+});
+
+app.use((req,res,next) => {
+    console.log("in another middleware");
+    res.send('<h1>hello form express</h1>');
+});
+
+const Server = http.createServer(app);
 
 Server.listen(3000, () => {console.log('Server listening on port 3000');});
