7c504ace1c7ae48a62b3dbfd010f0708f87360f7
vsbrall143 <vsbrall143@gmail.com>
2024-11-24 00:36:18 +0530

    routes are managed by routes folder, Router is used to attach respose and router is exported to the app.js file each route is filtered through /admin which is common segment for some routes for add-product page this all helps to make code clean

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-const http = require("http");   //core module import
 const express=require("express");
 const bodyParser=require("body-parser");   //3rd party package used for parsing autometically
 
-
 const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));  //shis middleware parsed the form input autometically it has built in next() function
 
-app.use("/add-product", (req,res,next) => {
+app.use('/admin',adminRoutes); // here '/admin' adds a filtering mechanism which add common starting segment which outsource form routes so that we dont have to repeteadly add /admin in all the paths over the admin.js file 
+app.use(shopRoutes);
+
+app.use( (req,res,next) => {
  
-    res.send(' <form action="/product" method="POST"><label for="title">Title:</label> <input type="text" name="title" id="title"> <label for="size">Size:</label> <input type="text" name="size" id="size"><button type="submit">Add Product</button></form>');
- 
+    res.status(404).send('<h1>Page Not Found</h1>');
 });
 
-app.post("/product", (req,res,next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
-
-
-app.use("/", (req,res,next) => {
- 
-    res.send('<h1>hello form express</h1>');
-});
 
 app.listen(3000);
